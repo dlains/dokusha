6f741aa6a85f1f5bc19ab4253dee129f4a6ad05b
David Lains <dlains@gmail.com>
2020-05-03 19:06:03 -0400

    Close #7 Added a starting Word model.

--- lib/dokusha.rb
+++ lib/dokusha.rb
@@ -3,6 +3,7 @@ require 'dokusha/meaning'
 require 'dokusha/radical'
 require 'dokusha/reading'
 require 'dokusha/version'
+require 'dokusha/word'
 
 # Ignore any error loading byebug if it isn't avalable.
 begin
